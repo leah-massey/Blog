f78d9f3eb244d2ea5d3c68e8f1ec4b843f039cc2
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-13 10:50:21 +0100

    feat:routes set up using express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = 3001;
+
+//listen for requests
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  //res.send("<p>home page</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  //res.send("<p>about page</p>");
+  res.sendFile("./views/about.html", { root: __dirname });
+});
