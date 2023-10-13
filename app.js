f161cd469e129e864b8dcf807f4ebb7ea4cf2eb6
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-13 12:32:31 +0100

    feat: create blog post page made

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
+
 // 404 page
 app.use((req, res) => {
   res.status(404).render("404");
