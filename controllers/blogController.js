497b06f296ddf42f41e4e9c2bf4a6a8420df29ab
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-15 14:41:25 +0100

    refactor: blog views moved to blog folder to allow for more sections in future (e.g comments)

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -6,7 +6,7 @@ const blog_index = (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
-      res.render("index", { title: "all Blogs", blogs: result });
+      res.render("blogs/index", { title: "all Blogs", blogs: result });
     })
     .catch((err) => {
       console.log(err);
@@ -17,7 +17,7 @@ const blog_details = (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
     .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
+      res.render("blogs/details", { blog: result, title: "Blog Details" });
     })
     .catch((err) => {
       console.log(err);
@@ -25,7 +25,7 @@ const blog_details = (req, res) => {
 };
 
 const blog_create_get = (req, res) => {
-  res.render("create", { title: "Create a new blog" });
+  res.render("blogs/create", { title: "Create a new blog" });
 };
 
 const blog_create_post = (req, res) => {
